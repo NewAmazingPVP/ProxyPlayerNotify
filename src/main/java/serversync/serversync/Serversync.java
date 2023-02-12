1cc48b14d9ef42b8230b5b7220f5279e040d671b
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2023-02-11 21:20:03 -0500

    Project is now public
    First push of the project

---
+++ src/main/java/serversync/serversync/Serversync.java
@@ -0,0 +1,71 @@
+package serversync.serversync;
+
+import java.io.File;
+import java.io.IOException;
+
+import net.md_5.bungee.api.event.PlayerDisconnectEvent;
+import net.md_5.bungee.api.event.ServerConnectEvent;
+import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.config.Configuration;
+import net.md_5.bungee.config.ConfigurationProvider;
+import net.md_5.bungee.config.YamlConfiguration;
+import net.md_5.bungee.event.EventHandler;
+
+public class Serversync extends Plugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        if (!getDataFolder().exists()) {
+            getDataFolder().mkdir();
+        }
+
+        File file = new File(getDataFolder(), "config.yml");
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+                Configuration configuration = ConfigurationProvider.getProvider(YamlConfiguration.class).load(file);
+                configuration.set("join_message", "%player% has joined the network!");
+                configuration.set("switch_message", "%player% has switched to server %server%");
+                configuration.set("leave_message", "%player% has left the network");
+                ConfigurationProvider.getProvider(YamlConfiguration.class).save(configuration, file);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        getProxy().getPluginManager().registerListener(this, this);
+    }
+
+    @EventHandler
+    public void onJoin(ServerConnectEvent event) {
+        String message = getConfig().getString("join_message");
+        message = message.replace("%player%", event.getPlayer().getName());
+        getProxy().broadcast(message);
+    }
+
+    @EventHandler
+    public void onSwitch(ServerConnectEvent event) {
+        String message = getConfig().getString("switch_switch");
+        message = message.replace("%player%", event.getPlayer().getName());
+        message = message.replace("%server%", event.getTarget().getName());
+        getProxy().broadcast(message);
+    }
+
+    @EventHandler
+    public void onLeave(ServerConnectEvent event) {
+        String message = getConfig().getString("leave_switch");
+        message = message.replace("%player%", event.getPlayer().getName());
+        message = message.replace("%server%", event.getTarget().getName());
+        getProxy().broadcast(message);
+    }
+
+    private Configuration getConfig() {
+        try {
+            return ConfigurationProvider.getProvider(YamlConfiguration.class).load(new File(getDataFolder(), "config.yml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
