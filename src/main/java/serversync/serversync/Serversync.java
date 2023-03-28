ec64b4a390146771b1be7ef722e939c942dd9b6b
NewAmazingPVP <gamingofpat@gmail.com>
2023-03-28 19:11:09 -0400

    Better format

--- src/main/java/serversync/serversync/Serversync.java
+++ src/main/java/serversync/serversync/Serversync.java
@@ -17,50 +17,52 @@ import net.md_5.bungee.event.EventHandler;
 
 public class Serversync extends Plugin implements Listener {
 
-    // Called when the plugin is enabled    
+    // Called when the plugin is enabled
     @Override
     public void onEnable() {
-        // Create the plugin data folder if it does not already exist        
+        // Create the plugin data folder if it does not already exist
         if (!getDataFolder().exists()) {
             getDataFolder().mkdir();
         }
 
-        // Create the plugin data folder if it does not exist        
+        // Create the plugin data folder if it does not exist
         File file = new File(getDataFolder(), "config.yml");
         if (!file.exists()) {
             try {
                 file.createNewFile();
 
-                // Set default configuration values                
+                // Set default configuration values
                 Configuration configuration = ConfigurationProvider.getProvider(YamlConfiguration.class).load(file);
                 configuration.set("join_message", "%player% has joined the network!");
                 configuration.set("switch_message", "%player% has switched/joined to the %server% server");
                 configuration.set("leave_message", "%player% has left the network");
                 configuration.set("color", "b");
 
-                // Save the configuration file                
+                // Save the configuration file
                 ConfigurationProvider.getProvider(YamlConfiguration.class).save(configuration, file);
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
-        
-        // Register the plugin's event listener        
+
+        // Register the plugin's event listener
         getProxy().getPluginManager().registerListener(this, this);
     }
 
-    //Called when a player joins the network
+    // Called when a player joins the network
     @EventHandler
     public void onJoin(PostLoginEvent event) {
-        // Get the join message from the config file and replace placeholders with actual values        
+        // Get the join message from the config file and replace placeholders with
+        // actual values
         String message = getConfig().getString("join_message");
         message = message.replace("%player%", event.getPlayer().getName());
 
-        // Broadcast the message to all servers in the network with the color chosen in the config file
+        // Broadcast the message to all servers in the network with the color chosen in
+        // the config file
         getProxy().broadcast("§" + getConfig().getString("color") + message);
     }
-    
-    // Called when a player switches servers in the network    
+
+    // Called when a player switches servers in the network
     @EventHandler
     public void onSwitch(ServerConnectedEvent event) {
         String message = getConfig().getString("switch_message");
@@ -69,7 +71,7 @@ public class Serversync extends Plugin implements Listener {
         getProxy().broadcast("§" + getConfig().getString("color") + message);
     }
 
-    // Called when a player leaves the network    
+    // Called when a player leaves the network
     @EventHandler
     public void onLeave(PlayerDisconnectEvent event) {
         String message = getConfig().getString("leave_message");
@@ -78,17 +80,17 @@ public class Serversync extends Plugin implements Listener {
     }
 
     private Configuration getConfig() {
-    // Attempt to load the config file as a YamlConfiguration object
-    try {
-        File configFile = new File(getDataFolder(), "config.yml");
-        Configuration configuration = ConfigurationProvider.getProvider(YamlConfiguration.class).load(configFile);
-        return configuration;
-    } catch (IOException e) {
-        // Print the stack trace if there is an error loading the file
-        e.printStackTrace();
+        // Attempt to load the config file as a YamlConfiguration object
+        try {
+            File configFile = new File(getDataFolder(), "config.yml");
+            Configuration configuration = ConfigurationProvider.getProvider(YamlConfiguration.class).load(configFile);
+            return configuration;
+        } catch (IOException e) {
+            // Print the stack trace if there is an error loading the file
+            e.printStackTrace();
+        }
+        // Return null if the configuration couldn't be loaded
+        return null;
     }
-    // Return null if the configuration couldn't be loaded
-    return null;
-}
 
 }
