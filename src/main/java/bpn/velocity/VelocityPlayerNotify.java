57edf147e313b362e9ec6cc65c0bf02e88b8fece
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2023-06-09 11:12:03 -0400

    config fixes for velocity

--- src/main/java/bpn/velocity/VelocityPlayerNotify.java
+++ src/main/java/bpn/velocity/VelocityPlayerNotify.java
@@ -1,6 +1,7 @@
 package bpn.velocity;
 
 import com.google.inject.Inject;
+import com.moandjiezana.toml.Toml;
 import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.connection.DisconnectEvent;
 import com.velocitypowered.api.event.player.PlayerChooseInitialServerEvent;
@@ -13,38 +14,48 @@ import net.luckperms.api.LuckPerms;
 import net.luckperms.api.LuckPermsProvider;
 import net.luckperms.api.model.user.User;
 import ninja.leaping.configurate.ConfigurationNode;
-import ninja.leaping.configurate.hocon.HoconConfigurationLoader;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 @Plugin(id = "bungeeplayernotify", name = "ProxyPlayerNotify", authors = "NewAmazingPVP")
 public class VelocityPlayerNotify {
 
-    private ConfigurationNode config;
+    private Toml config;
     private LuckPerms luckPerms;
     private final ProxyServer proxy;
     private final Path dataDirectory;
 
     @Inject
-    public VelocityPlayerNotify(ProxyServer server, @DataDirectory Path dataDirectory) {
-        this.proxy = server;
+    public VelocityPlayerNotify(ProxyServer proxy, @DataDirectory Path dataDirectory) {
+        this.proxy = proxy;
         this.dataDirectory = dataDirectory;
+        config = loadConfig(dataDirectory);
     }
 
-    public void saveDefaultConfig() throws IOException {
-        Path configFile = dataDirectory.resolve("config.conf");
-        if (!Files.exists(configFile)) {
-            Files.createDirectories(configFile.getParent());
-            Files.copy(getClass().getResourceAsStream("/config.conf"), configFile);
+    private Toml loadConfig(Path path) {
+        File folder = path.toFile();
+        File file = new File(folder, "config.toml");
+
+        if (!file.exists()) {
+            if (!file.getParentFile().exists()) {
+                file.getParentFile().mkdirs();
+            }
+            try (InputStream input = getClass().getResourceAsStream("/" + file.getName())) {
+                if (input != null) {
+                    Files.copy(input, file.toPath());
+                } else {
+                    file.createNewFile();
+                }
+            } catch (IOException exception) {
+                exception.printStackTrace();
+                return null;
+            }
         }
-    }
-
-    public void loadConfig() throws IOException {
-        Path configFile = dataDirectory.resolve("config.conf");
-        HoconConfigurationLoader loader = HoconConfigurationLoader.builder().setPath(configFile).build();
-        config = loader.load();
+        return new Toml().read(file);
     }
 
     @Subscribe
@@ -59,19 +70,21 @@ public class VelocityPlayerNotify {
 
     @Subscribe
     public void onLeave(DisconnectEvent event) {
+
         sendMessage("leave_message", event.getPlayer(), null, null);
     }
 
     public void sendMessage(String type, Player targetPlayer, String connectedServer, String disconnectedServer) {
-        if (config.getNode("permission", "permissions").getBoolean()) {
-            if (config.getNode("permission", "notify_message").getBoolean()) {
+        if (config.getBoolean(("permission.permissions"))) {
+            if (config.getBoolean("permission.notify_message")) {
                 if (targetPlayer.hasPermission("bpn.notify")) {
-                    String finalMessage = config.getNode(type).getString().replace("%player%", targetPlayer.getUsername());
+                    String finalMessage = config.getString(type).replace("%player%", targetPlayer.getUsername());
                     if (finalMessage.equals("")) {
                         return;
                     }
                     if (type.equals("switch_message")) {
-                        finalMessage = finalMessage.replace("%server%", connectedServer);
+                        finalMessage = finalMessage.replace("%connectedServer%", connectedServer);
+                        finalMessage = finalMessage.replace("%previousServer%", disconnectedServer);
                     }
                     finalMessage = finalMessage.replace("&", "§");
                     if (finalMessage.contains("%lp_prefix%")) {
@@ -83,18 +96,18 @@ public class VelocityPlayerNotify {
                             finalMessage = finalMessage.replace("%lp_prefix%", prefix);
                         }
                     }
-                    if (config.getNode("permission", "hide_message").getBoolean()) {
+                    if (config.getBoolean("permission.hide_message")) {
                         for (Player pl : proxy.getAllPlayers()) {
                             if (pl.hasPermission("bpn.view")) {
                                 pl.sendMessage(finalMessage);
                             }
                         }
                     } else {
-                        proxy.getAllPlayers().forEach(player -> player.sendMessage(finalMessage))
+                        proxy.getAllPlayers().forEach(player -> player.sendMessage(finalMessage));
                     }
                 }
-            } else if (config.getNode("permission", "hide_message").getBoolean()) {
-                String finalMessage = config.getNode(type).getString().replace("%player%", targetPlayer.getUsername());
+            } else if (config.getBoolean("permission.hide_message")) {
+                String finalMessage = config.getString(type).replace("%player%", targetPlayer.getUsername());
                 if (finalMessage.equals("")) {
                     return;
                 }
@@ -114,6 +127,7 @@ public class VelocityPlayerNotify {
                 for (Player pl : proxy.getAllPlayers()) {
                     if (pl.hasPermission("bpn.view")) {
                         pl.sendMessage(finalMessage);
+                        
                     }
                 }
             }
