b9ff834c39b7911ccd192d250f01f86f033411d3
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2024-09-02 10:30:48 -0400

    fix disconnect intended messages
    fix disconnect intended messages

--- src/main/java/bpn/velocity/VelocityPlayerNotify.java
+++ src/main/java/bpn/velocity/VelocityPlayerNotify.java
@@ -158,6 +158,7 @@ public class VelocityPlayerNotify {
             if (limboServers != null && lastServer != null && limboServers.contains(lastServer.toLowerCase())) {
                 return;
             }
+            if(lastServer == null) return;
             sendMessage("leave_message", player, null, lastServer);
         }
     }
